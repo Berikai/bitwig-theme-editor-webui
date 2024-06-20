0a24380d7f9b6280c5c12ab06b21a0a708ff6466
Berikai <berkayeren2009@gmail.com>
2024-06-20 16:18:24 +0300

    feat: add shadow

---
+++ src/lib/SideBar/SideBar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Values from "./Values.svelte";
+</script>
+
+<div>
+   <Values />
+</div>
+
+<style>
+    div {
+        --sidebar-width: 350px;
+        border-right: 1px solid #000000;
+        box-shadow: 2px 0px 4px rgba(0, 0, 0, 0.2);
+        z-index: 3;
+        height: 100%;
+        width: var(--sidebar-width);
+    }
+</style>
