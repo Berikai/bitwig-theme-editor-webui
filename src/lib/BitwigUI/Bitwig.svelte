2fbf1502b10ba826287be0f7fc7bb23e3ac2e82c
Berikai <berkayeren2009@gmail.com>
2024-06-20 17:39:25 +0300

    feat: add bottom bar

--- src/lib/BitwigUI/Bitwig.svelte
+++ src/lib/BitwigUI/Bitwig.svelte
@@ -4,6 +4,7 @@
     import WindowBar from './WindowBar/WindowBar.svelte';
 
     import { theme } from "../Theme";
+    import Bottom from './Bottom/Bottom.svelte';
 
     let themeValue;
 
@@ -11,12 +12,16 @@
         themeValue = _value;
     });
 
-    const showReference = 0;
+    const showReference = 0; // Development only, set to 0 in production
 </script>
 
 <div id="bitwig" style="{showReference ? `background-image: url(${Reference});` : ""} background-color: {themeValue["window"]["Window background"]};">
     <TopBar />
     <WindowBar />
+    <span>
+        <slot></slot>
+    </span>
+    <Bottom />
 </div>
 
 <style>
@@ -35,5 +40,19 @@
         width: var(--width);
         height: var(--height);
         box-shadow: #202020 0px 0px 15px;
+    }
+    span {
+        --side-margin: 5px;
+        width: calc(100% - calc(2 * var(--side-margin)));
+        height: 656px;
+        display: flex;
+        flex-direction: row;
+        background-color: #00ff00;
+
+        margin-top: 2px;
+        margin-bottom: 2px;
+        margin-left: 5px;
+
+        opacity: 0.5; /*Development only, set to 1 in production */
     }
 </style>
