27c3d052040ad7d436619d555ef88eef8c5e3fdb
Berikai <berkayeren2009@gmail.com>
2024-06-21 14:25:22 +0300

    feat: add small screen error

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,23 @@
     import Content from "./lib/Content.svelte";
     import Sidebar from "./lib/SideBar/SideBar.svelte";
     import Topbar from "./lib/TopBar/TopBar.svelte";
+    import ContentBar from "./lib/ContentBar.svelte";
 </script>
 
 <main>
     <Topbar />
     <div>
         <Sidebar />
+        <ContentBar />
         <Content />
     </div>
 </main>
+
+<div class="smallscreen">
+    <h2>Bitwig Theme Editor WebUI</h2>
+    <p>Your screen resolution is too low.</p>
+    <p>Please use a resolution with minimum 720px width.</p>
+</div>
 
 <style>
     main {
@@ -22,8 +30,24 @@
     }
     div {
         --topbar-height: 40px;
+        position: relative;
         display: flex;
         flex-direction: row;
         height: calc(100% - var(--topbar-height));
+    }
+    @media only screen and (max-width: 719px) {
+        main { display: none; }
+        .smallscreen { 
+            display: flex;
+            height: calc(100vh - 2*10px);
+            flex-direction: column;
+            padding: 10px;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+    }
+    @media only screen and (min-width: 720px) {
+        .smallscreen { display: none; }
     }
 </style>
