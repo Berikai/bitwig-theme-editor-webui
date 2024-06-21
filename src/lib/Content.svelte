436314ca65f6d24999d1bfa8066b243be4b0b1d8
Berikai <berkayeren2009@gmail.com>
2024-06-21 14:25:44 +0300

    feat: add padding to content space

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -1,22 +1,21 @@
 <script>
     import Bitwig from './BitwigUI/Bitwig.svelte';
     import Transparent from '../assets/transparent.png';
-    //import Contentbar from './ContentBar.svelte';
 </script>
 
 <div style="background-image: url({Transparent});">
-    <!-- <Contentbar /> -->
     <Bitwig />
 </div>
 
 <style>
     div {
         --sidebar-width: 350px;
-        
-        height: 100%;
+        --content-padding: 10px;
+        /*height: 100% + var(--content-padding);*/
         display: flex;
-        position: relative;
         width: calc(100% - var(--sidebar-width));
         overflow: auto;
+        padding: var(--content-padding);
+        scrollbar-color: #444 #333;
     }
 </style>
