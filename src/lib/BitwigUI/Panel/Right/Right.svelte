a0cef3096abe67731db813a80b9ac40fa10577a5
Berikai <berkayeren2009@gmail.com>
2024-06-26 15:15:33 +0300

    feat(browser): add svg

--- src/lib/BitwigUI/Panel/Right/Right.svelte
+++ src/lib/BitwigUI/Panel/Right/Right.svelte
@@ -2,6 +2,8 @@
     import { onMount } from "svelte";
     import { ThemeStore } from "../../../Theme";
     import { scrollToValue } from '../../../scrollToValue';
+    
+    import Any from "./Icons/Any.svelte";
 
     onMount(() => {
         document.getElementById("browser-panel").addEventListener("mousedown", (e) => {
@@ -26,22 +28,32 @@
     <span class="top-nav"></span>
     <span class="everything-bar">
         <i>Everything</i>
-        <span class="search-bar hole-dark"><i class="text">Search</i></span>
+        <span class="search-bar hole-dark">
+            <span style="display: flex;align-items:center;">
+                <svg x="0px" y="0px" width="17" height="11" fill={"#cfcfcf"} viewBox="0 0 17 11" style="margin-left:5px;"><path d="M 10.643,9.49 8.265,7.112 C 8.664,6.486 8.881,5.76 8.881,5 8.881,3.947 8.471,2.958 7.727,2.213 6.982,1.469 5.993,1.06 4.94,1.06 3.887,1.06 2.898,1.47 2.154,2.214 1.41,2.958 1,3.947 1,5 1,6.053 1.41,7.042 2.154,7.786 2.898,8.53 3.888,8.939 4.94,8.939 5.701,8.939 6.426,8.722 7.052,8.324 L 9.431,10.702 10.643,9.49 z M 3.002,6.938 C 2.484,6.42 2.199,5.732 2.199,5 2.199,4.268 2.484,3.58 3.002,3.062 3.52,2.544 4.208,2.259 4.94,2.259 5.671,2.259 6.36,2.544 6.878,3.062 7.396,3.58 7.682,4.268 7.682,5 7.682,5.732 7.397,6.42 6.879,6.938 6.36,7.456 5.672,7.74 4.94,7.74 4.208,7.74 3.52,7.456 3.002,6.938 z"/><path style="fill-opacity:1;stroke:none" d="M 9.5580583,6 15.370019,6 12.464038,9.3535534 z"/></svg>
+                <i class="text">Search</i>
+            </span>
+            <svg width="7" height="7" viewBox="0 0 7 7" style="margin-right: 10px;"><line x1="0" y1="0" x2="7" y2="7" stroke="#c3c3c3" /><line x1="0" y1="7" x2="7" y2="0" stroke="#c3c3c3" /></svg>
+        </span>
         <span class="locations">
             <span class="location-side">
-                <i>Location</i>
-                <i>Category</i>
-                <i>Device</i>
+                <i><Any />Location</i>
+                <i><Any />Category</i>
+                <i><Any />Device</i>
             </span>
             <span class="location-side">
-                <i>File Kind</i>
-                <i>Creator</i>
-                <i>Tags</i>
+                <i><Any />File Kind</i>
+                <i><Any />Creator</i>
+                <i><Any />Tags</i>
             </span>
         </span>
     </span>
-    <span class="files hole-dark"></span>
-    <span class="bottom-bar"></span>
+    <span class="files hole-dark">
+        
+    </span>
+    <span class="bottom-bar">
+        
+    </span>
 </div>
 
 <style>
@@ -53,9 +65,16 @@
         border-radius: 3px;
         border: 2px solid var(--panel-stroke);
         overflow: hidden;
+
+        opacity: 1; /*Development only, set to 1 in production */
+    }
+    i {
+        display: flex;
+        align-items: center;
+
     }
     .text {
-        margin-left: 20px;
+        margin-left: 2px;
     }
     .top-nav {
         display: flex;
@@ -75,6 +94,7 @@
     .search-bar {
         display: flex;
         align-items: center;
+        justify-content: space-between;
         font-size: 11px;
         margin-top: 6px;
         width: 204px;
@@ -96,7 +116,7 @@
         justify-content: space-around;
         width: 100px;
         height: 70px;
-        padding-left: 24px;
+        padding-left: 0px; /* 24px; */
     }
     .files {
         display: flex;
